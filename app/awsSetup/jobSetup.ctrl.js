31cd76bc5bcef4a640ba72e0b27b2830e53ab436
Aliasguru <message@aliasguru.eu>
2017-10-13 14:49:16 +0200

    + update work template

--- app/awsSetup/jobSetup.ctrl.js
+++ app/awsSetup/jobSetup.ctrl.js
@@ -44,11 +44,12 @@ angular.module('awsSetup')
             'EOF\n'
         ].join('');
 
-        $scope.workTemplateFullframe = 'blender -b *.blend -F PNG -o $OUTDIR/frame_###### -s $START -e $END -j $STEP -t 0 -a';
+        $scope.workTemplateFullframe = 'blender -b *.blend -F MULTILAYER -y --python-expr \"import bpy;bpy.ops.configurator.set_sampling(scene=\'ALL\', device=\'GPU\', samples=25, percentage=100, branched=False, clamping=True, max_bounces=20, min_bounces=10,transparent_max_bounces=15,transparent_min_bounces=15)\" -S YOURSCENEHERE -o $OUTDIR/render_### -s $START -e $END -j $STEP -t 0 -a';
+        //$scope.workTemplateFullframe = 'blender -b *.blend -F MULTILAYER -y -S YOURSCENEHERE -o $OUTDIR/render_### -s $START -e $END -j $STEP -t 0 -a';
         $scope.workTemplateSubframe = 'blender -b *.blend $SCRIPT -F PNG -o $OUTDIR/frame_######_x$SF_MIN_Xto$SF_MAX_Xy$SF_MIN_Yto$SF_MAX_Y -s $START -e $END -j $STEP -t 0 -a';
         $scope.workTemplate = $scope.workTemplateFullframe;
         $scope.startFrame = 1;
-        $scope.endFrame = 240;
+        $scope.endFrame = 9;
 
         $scope.shuffle = Boolean(localStorageService.get('shuffleQ'));
     };
